e26951e827655003c19dfb92c7453ed94eb6d5ae
Frank Schwab <xformerfhs@users.noreply.github.com>
2023-08-10 19:46:42 +0200

    Refelct new ownership

--- PBKDF2Java.java
+++ PBKDF2Java.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2016, DB Systel GmbH
+ * Copyright (c) 2016-2023, DB Systel GmbH
+ * Copyright (c) 2023, Frank Schwab
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -17,7 +18,7 @@
  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * Author: Frank Schwab, DB Systel GmbH
+ * Author: Frank Schwab
  *
  * Example program to show correct and incorrect password storage with the PBKDF2 function
  *
